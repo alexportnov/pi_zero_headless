866c60ac082889798dbca6efbd792cdd190c6799
Alex <alex@everysight.com>
2019-07-20 17:25:05 +0300

    + Kernel: defconfig removing unneded configs

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 source common.sh
 
 # apply patches ...
-find $RUNDIR/patches -type f \( -name "*.patch" -o -name "*.diff" \) | sort | while read f; do p=`echo $f | awk -F"/" '{for(i=1;i<NF;i++){printf "%s/", $i} }' `; echo $f; cat $f | patch -p1 -N -d $RUNDIR/$p; done;
-
+set +e
+cd $RUNDIR/patches
+find . -type f \( -name "*.patch" -o -name "*.diff" \) | sort | while read f; do p=`echo $f | awk -F"/" '{for(i=1;i<NF;i++){printf "%s/", $i} }' `; echo $f; cat $f | patch -p1 -N -d $RUNDIR/$p; done;
+set -e
 
 # build kernel and modules
 cd $KERNEL_SOURCE
